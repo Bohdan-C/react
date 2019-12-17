167b3ac1e2f625de51facef1b0fdc704b4d0bb6e
Bohdan <bohdanchurk31@gmail.com>
2019-12-17 15:41:38 +0200

    first-commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Post from "../components/post/Post";
+
+const App = () => (
+  <>
+    <Post
+      title="nice day"
+      image="https://rbkgames.com/files/editor_uploads/Rins/gta/%D0%9D%D0%BE%D0%B2%D0%B0%D1%8F%D0%9F%D0%B0%D0%BF%D0%BA%D0%B0%2039/csgo1.jpg"
+    />
+  </>
+);
+
+export default App;
---
+++ src/components/post/Post.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Post = props => {
+  console.log(props);
+  return <>{/* <h2>{title}</h2> */}</>;
+};
+
+export default Post;
